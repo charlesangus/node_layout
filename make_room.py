78e98467dab87ffb4ec6451f3dd7f96e62551777
Charles A Taylor <c.angustaylor@gmail.com>
2026-03-02 08:22:32 -0500

    add make room commands

---
+++ make_room.py
@@ -0,0 +1,40 @@
+import nuke
+
+
+def make_room(amount=1600, direction="up"):
+    nodes_to_move = nuke.selectedNodes()
+
+    if direction == "up":
+        x_amount = 0
+        y_amount = -amount
+    elif direction == "down":
+        y_amount = amount
+        x_amount = 0
+
+    if direction == "left":
+        y_amount = 0
+        x_amount = -amount
+    elif direction == "right":
+        y_amount = 0
+        x_amount = amount
+
+    # override nodes to move if no selection
+    if len(nodes_to_move) == 0:
+        nukescripts.clear_selection_recursive()
+
+        n = nuke.createNode("Dot", inpanel=False)
+        y = n.ypos()
+        nuke.delete(n)
+
+        if direction == "up":
+            nodes_to_move = [node for node in nuke.allNodes() if node.ypos() < y]
+        elif direction == "down":
+            nodes_to_move = [node for node in nuke.allNodes() if node.ypos() > y]
+        elif direction in ("left", "right"):
+            nodes_to_move = []
+
+
+    for node in nodes_to_move:
+        node.setXpos(node.xpos() + x_amount)
+        node.setYpos(node.ypos() + y_amount)
+
--- menu.py
+++ menu.py
@@ -1,9 +1,22 @@
 import nuke
 import node_layout
+import make_room
 
-nuke.menu('Nuke').addCommand(
-    'Edit/Layout Upstream',
+
+m = nuke.menu("Nuke")
+
+m.addCommand(
+    'Edit/Node Layout/Layout Upstream',
     node_layout.layout_upstream,
     'shift+l',
     shortcutContext=2,
 )
+
+m.addCommand("Edit/Node Layout/Make Room Above", "make_room.make_room()", "[", shortcutContext=2,)
+m.addCommand("Edit/Node Layout/Make Room Below", "make_room.make_room(direction='down')", "]", shortcutContext=2,)
+
+m.addCommand("Edit/Node Layout/Make Room Above (smaller)", "make_room.make_room(amount=800)", "Ctrl+[", shortcutContext=2,)
+m.addCommand("Edit/Node Layout/Make Room Below (smaller)", "make_room.make_room(amount=800, direction='down')", "Ctrl+]", shortcutContext=2,)
+
+m.addCommand("Edit/Node Layout/Make Room Left", "make_room.make_room(direction='left')", "{", shortcutContext=2,)
+m.addCommand("Edit/Node Layout/Make Room Right", "make_room.make_room(direction='right')", "}", shortcutContext=2,)
