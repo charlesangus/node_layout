481339375a65431cd441d7ac9f7a618afdd62c1a
Charles A Taylor <c.angustaylor@gmail.com>
2026-03-02 08:29:55 -0500

    add util commands

--- menu.py
+++ menu.py
@@ -4,19 +4,28 @@ import make_room
 
 
 m = nuke.menu("Nuke")
+edit = m.findItem("Edit")
+layout_menu = edit.addMenu("Node Layout")
 
-m.addCommand(
-    'Edit/Node Layout/Layout Upstream',
+layout_menu.addCommand(
+    'Layout Upstream',
     node_layout.layout_upstream,
     'shift+l',
     shortcutContext=2,
 )
 
-m.addCommand("Edit/Node Layout/Make Room Above", "make_room.make_room()", "[", shortcutContext=2,)
-m.addCommand("Edit/Node Layout/Make Room Below", "make_room.make_room(direction='down')", "]", shortcutContext=2,)
+layout_menu.addSeparator()
 
-m.addCommand("Edit/Node Layout/Make Room Above (smaller)", "make_room.make_room(amount=800)", "Ctrl+[", shortcutContext=2,)
-m.addCommand("Edit/Node Layout/Make Room Below (smaller)", "make_room.make_room(amount=800, direction='down')", "Ctrl+]", shortcutContext=2,)
+layout_menu.addCommand("Sort By Filename", "sort_by_filename()", shortcutContext=2,)
+layout_menu.addCommand("Select Upstream Ignoring Hidden", "select_upstream_ignoring_hidden()", "E", shortcutContext=2,)
 
-m.addCommand("Edit/Node Layout/Make Room Left", "make_room.make_room(direction='left')", "{", shortcutContext=2,)
-m.addCommand("Edit/Node Layout/Make Room Right", "make_room.make_room(direction='right')", "}", shortcutContext=2,)
+layout_menu.addSeparator()
+
+layout_menu.addCommand("Make Room Above", "make_room.make_room()", "[", shortcutContext=2,)
+layout_menu.addCommand("Make Room Below", "make_room.make_room(direction='down')", "]", shortcutContext=2,)
+
+layout_menu.addCommand("Make Room Above (smaller)", "make_room.make_room(amount=800)", "Ctrl+[", shortcutContext=2,)
+layout_menu.addCommand("Make Room Below (smaller)", "make_room.make_room(amount=800, direction='down')", "Ctrl+]", shortcutContext=2,)
+
+layout_menu.addCommand("Make Room Left", "make_room.make_room(direction='left')", "{", shortcutContext=2,)
+layout_menu.addCommand("Make Room Right", "make_room.make_room(direction='right')", "}", shortcutContext=2,)
---
+++ util.py
@@ -0,0 +1,42 @@
+import nuke
+
+
+def sort_by_filename():
+    ns = nuke.selectedNodes()
+
+    ns = [n for n in ns if "file" in n.knobs()]
+
+    ns = sorted(ns, key=lambda n: n["file"].value())
+
+    start_location = (min([n.xpos() for n in ns]), min([n.ypos() for n in ns]))
+
+    print(start_location)
+
+    interval = 300
+
+    for i, n in enumerate(ns):
+        n.setXYpos(start_location[0] + interval * i, start_location[1])
+
+
+def upstream_ignoring_hidden(node, nodes_so_far=None):
+    inputs = node.dependencies(what=nuke.INPUTS)
+    if len(inputs) == 0:
+        return nodes_so_far
+    else:
+        if nodes_so_far == None:
+            nodes_so_far = set(inputs)
+        else:
+            nodes_so_far.update(set(inputs))
+    for input in inputs:
+        nodes_so_far.update(upstream_ignoring_hidden(input, nodes_so_far))
+    return nodes_so_far
+
+
+def select_upstream_ignoring_hidden():
+    node = nuke.selectedNode()
+    ns = upstream_ignoring_hidden(node)
+    nukescripts.clear_selection_recursive()
+    for n in ns:
+        n["selected"].setValue(True)
+    node["selected"].setValue(True)
+
