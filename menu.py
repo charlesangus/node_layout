616dae717d330a2e8e64c5e4e15b9ac108aa23be
Charles A Taylor <c.angustaylor@gmail.com>
2026-03-03 00:05:10 -0500

    Fix bugs in menu.py: import util, prefix commands, fix sdirection typo

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import nuke
 import node_layout
 import make_room
+import util
 
 
 m = nuke.menu("Nuke")
@@ -10,14 +11,14 @@ layout_menu = edit.addMenu("Node Layout")
 layout_menu.addCommand(
     'Layout Upstream',
     node_layout.layout_upstream,
-    'shift+l',
+    'shift+e',
     shortcutContext=2,
 )
 
 layout_menu.addSeparator()
 
-layout_menu.addCommand("Sort By Filename", "sort_by_filename()", shortcutContext=2,)
-layout_menu.addCommand("Select Upstream Ignoring Hidden", "select_upstream_ignoring_hidden()", "E", shortcutContext=2,)
+layout_menu.addCommand("Sort By Filename", "util.sort_by_filename()", shortcutContext=2,)
+layout_menu.addCommand("Select Upstream Ignoring Hidden", "util.select_upstream_ignoring_hidden()", "E", shortcutContext=2,)
 
 layout_menu.addSeparator()
 
@@ -27,5 +28,5 @@ layout_menu.addCommand("Make Room Below", "make_room.make_room(direction='down')
 layout_menu.addCommand("Make Room Above (smaller)", "make_room.make_room(amount=800)", "Ctrl+[", shortcutContext=2,)
 layout_menu.addCommand("Make Room Below (smaller)", "make_room.make_room(amount=800, direction='down')", "Ctrl+]", shortcutContext=2,)
 
-layout_menu.addCommand("Make Room Left", "make_room.make_room(direction='left')", "{", shortcutContext=2,)
-layout_menu.addCommand("Make Room Right", "make_room.make_room(direction='right')", "}", shortcutContext=2,)
+layout_menu.addCommand("Make Room Left", "make_room.make_room(amount=800, direction='left')", "{", shortcutContext=2,)
+layout_menu.addCommand("Make Room Right", "make_room.make_room(amount=800, direction='right')", "}", shortcutContext=2,)
